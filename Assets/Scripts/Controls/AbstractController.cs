55fdb018751ae229f11a8e28efbf298b484b2e67
Artem Bishev <biart95@gmail.com>
2017-11-18 15:17:46 +0100

    Change the scripts structure and add a few scripts

---
+++ Assets/Scripts/Controls/AbstractController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game.Controls
+{
+    abstract public class AbstractController<T> : MonoBehaviour
+    {
+        public class OnChangeEvent : UnityEvent<T> { };
+        public OnChangeEvent OnChange = new OnChangeEvent();
+
+        abstract protected T ReadControls(float timeDelta);
+
+        public void Update()
+        {
+            if (isActiveAndEnabled)
+            {
+                OnChange.Invoke(ReadControls(Time.deltaTime));
+            }
+        }
+    }
+
+    // This instance is defined in order to expose controllers to UnityEditor.
+    [System.Serializable]
+    abstract public class AbstractControllerVector2 : AbstractController<Vector2> { }
+}
---
+++ Assets/Scripts/Controls/InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Controls
+{
+    [AddComponentMenu("Controls/InputController")]
+    public class InputController : AbstractControllerVector2
+    {
+        public bool normalize = false;
+
+        public string axisXName = "Mouse X";
+        public string axisYName = "Mouse Y";
+
+        protected override Vector2 ReadControls(float timeDelta)
+        {
+            Vector2 input = new Vector2(
+                Input.GetAxis(axisXName),
+                Input.GetAxis(axisYName));
+            if (normalize)
+                input.Normalize();
+            return timeDelta * input;
+        }
+    }
+}
