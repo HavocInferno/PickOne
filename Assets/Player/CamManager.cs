2da84a0a430412c37ccb4ab3718c45d5ede96693
Paul Preissner HOME <paul@preissner-muc.de>
2017-11-19 20:38:10 +0100

    paul branch, changes in description
    this has:
    - lobby
    - vr checks, vr setup (basic)
    - enemy "ai" (follows nearest player in sight & range)

---
+++ Assets/Player/CamManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour {
+
+	public GameObject nonVRCamera;
+	public GameObject vrCamera;
+
+}
---
+++ Assets/Player/FollowTargetAI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowTargetAI : NetworkBehaviour
+{
+	public List<Transform> targets;
+	private Transform bestTarget;
+	private float closestTargetRange;
+
+	public float scanRate;
+	private float lastScan;
+	public float maxScanRange;
+
+	public LayerMask mask;
+
+	void Start() {
+		if (!isServer)
+			return;
+
+		targets = FindObjectOfType<playerlist> ().players;
+		/*targets.Clear ();
+		if (targets.Count == 0) {
+			foreach (CrawlerController cc in FindObjectsOfType<CrawlerController> ()) {
+				if(!cc.isVRMasterPlayer)
+					targets.Add (cc.gameObject.transform);
+			}
+		}*/
+	}
+
+	void FixedUpdate ()
+    {
+		if (!isServer)
+			return;
+
+		if (Time.time > lastScan) {
+			lastScan = Time.time + scanRate;
+
+			bestTarget = null;
+			closestTargetRange = maxScanRange + 1f;
+			foreach (Transform t in targets) {
+				float dist = (transform.position - t.position).magnitude;
+				if (dist < maxScanRange) {
+					Vector3 raycastDir = t.position - transform.position;
+					RaycastHit hit;
+					if (Physics.Raycast (
+						    transform.position,
+						    raycastDir,
+						    out hit,
+							maxScanRange,
+							mask) 
+						&& hit.collider.CompareTag("Crawler")) 
+					{
+						//Debug.Log("In sight: " + hit.collider.gameObject.name);
+						if (dist < closestTargetRange) {
+							closestTargetRange = dist;
+							bestTarget = t;
+						}
+					} else {
+						//Debug.Log ("No crawler in sight");
+					}
+				}
+			}
+			if(bestTarget != null) 
+				GetComponent<NavMeshAgent> ().destination = bestTarget.localPosition;
+		}
+    }
+}
