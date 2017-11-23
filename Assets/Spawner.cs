d248c98e7a7b685d57ac96353a2f6cebd4a45059
Artem Bishev <biart95@gmail.com>
2017-11-24 00:13:53 +0100

    Perform few tweaks regarding previous merge

---
+++ Assets/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Spawns objects randomly in specified area.
+/// </summary>
+public class Spawner : NetworkBehaviour
+{
+	public GameObject prefab;
+	public int numberOfEnemies;
+    public Rect area = new Rect(-8.0f, -8.0f, 16.0f, 16.0f);
+
+    GameObject Spawn()
+    {
+        var spawnPosition = new Vector3(
+            Random.Range(area.xMin, area.xMax),
+            0.0f,
+            Random.Range(area.yMin, area.yMax));
+
+        var spawnRotation = Quaternion.Euler(
+            0.0f,
+            Random.Range(0, 360),
+            0.0f);
+
+        var enemy = Instantiate(prefab, spawnPosition, spawnRotation);
+        NetworkServer.Spawn(enemy);
+        return enemy;
+    }
+
+	public override void OnStartServer()
+	{
+		for (int i = 0; i < numberOfEnemies; i++) Spawn();
+	}
+}
