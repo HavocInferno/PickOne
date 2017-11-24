1df63ac9439f2c7f2238358e67747d644bc0571f
Artem Bishev <biart95@gmail.com>
2017-11-25 00:46:32 +0100

    Add invisibility ability for crawlers

---
+++ Assets/Enemy/CrawlerAbility.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class CrawlerAbility : ScriptableObject
+{
+    public float cost = 0.0f;
+
+    virtual public void Activate(Crawler crawler)
+    {
+    }
+
+    virtual public void Deactivate(Crawler crawler)
+    {
+    }
+}
