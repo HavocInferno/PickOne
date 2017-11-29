c0479410b8679fbf31da89924cf875c9fed7829f
Paul Preissner HOME <paul@preissner-muc.de>
2017-11-29 11:02:36 +0100

    main setup fixed once more, +end UI
    i dont know how the hell i screwed up the main level so hard. but now it
    should work properly again.

---
+++ Assets/EndScreenUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScreenUI : MonoBehaviour {
+
+	public Text conditionLabel;
+
+	public void OnRetToLobbyClicked()
+	{
+		Debug.Log("a CLIENT wants to return to lobby");
+	}
+}
