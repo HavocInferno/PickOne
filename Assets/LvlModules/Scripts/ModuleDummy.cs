70eb926c139bfcd6a73f265ec2c44ba2ab1e48c9
Paul Preissner HOME <paul@preissner-muc.de>
2017-11-30 17:54:32 +0100

    interim lvl rev0.1

---
+++ Assets/LvlModules/Scripts/ModuleDummy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ModuleDummy : MonoBehaviour {
+
+	public Rect area = new Rect(0f, 0f, 18f, 18f);
+	public float height = 8f;
+	public Color gizmoColor = new Color(1, 0, 0, 0.13f);
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(area.width, height, area.height));
+
+		if (!Selection.Contains (gameObject)) {
+			Gizmos.color = gizmoColor;
+			Gizmos.DrawCube (transform.position, new Vector3 (area.width, height, area.height));
+		}
+
+		Gizmos.DrawLine(transform.position - Vector3.up * 1f, transform.position + Vector3.up * 1f);
+		Gizmos.DrawLine(transform.position - Vector3.forward * 1f, transform.position + Vector3.forward * 1f);
+		Gizmos.DrawLine(transform.position - Vector3.left * 1f, transform.position + Vector3.left * 1f);
+	}
+}
--- Assets/Player/CameraManager.cs
+++ Assets/Player/CameraManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class CameraManager : MonoBehaviour
 {
@@ -10,4 +11,18 @@ public class CameraManager : MonoBehaviour
 	public GameObject nonVRCamera;
 	public GameObject vrCamera;
 
+
+	[MenuItem("PickOneTools/Cams to crawler setup")]
+	private static void CrawlerCamsOption()
+	{
+		FindObjectOfType<CameraManager> ().nonVRCamera.SetActive (true);
+		FindObjectOfType<CameraManager> ().vrCamera.SetActive (false);
+	}
+
+	[MenuItem("PickOneTools/Cams to VR master setup")]
+	private static void MasterCamsOption()
+	{
+		FindObjectOfType<CameraManager> ().nonVRCamera.SetActive (false);
+		FindObjectOfType<CameraManager> ().vrCamera.SetActive (true);
+	}
 }
