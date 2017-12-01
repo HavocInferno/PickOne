306885cd09ce57f09b289e2b91ed9a992a116fe1
Artem Bishev <biart95@gmail.com>
2017-12-01 22:47:08 +0100

    Replace material when go invisible

--- Assets/Entities/Crawlers/Abilities/CloakEffect.cs
+++ Assets/Entities/Crawlers/Abilities/CloakEffect.cs
@@ -5,25 +5,45 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Assets/Effects/CloakEffect")]
 public class CloakEffect : AbstractEffect
 {
-    public Material cloakMaterial;      // Cloak material
+    public Material cloakMaterial;
 
     public override void Enable(GenericCharacter character)
     {
         base.Enable(character);
-        
-        var detectionComponent =
-            character.gameObject.GetComponent<DetectableObject>();
-        if (detectionComponent != null)
-            detectionComponent.isVisuallyDetectable = false;
+
+        character.gameObject.AddComponent<_CloakEffectScript>()._Initialize(cloakMaterial);
     }
 
     public override void Disable(GenericCharacter character)
     {
         base.Disable(character);
 
-        var detectionComponent =
-            character.gameObject.GetComponent<DetectableObject>();
-        if (detectionComponent != null)
-            detectionComponent.isVisuallyDetectable = true;
+        Destroy(character.gameObject.GetComponent<_CloakEffectScript>());
+    }
+
+    [RequireComponent(typeof(GenericCharacter))]
+    private class _CloakEffectScript : MonoBehaviour
+    {
+        Material oldMaterial = null; 
+        public void _Initialize(Material cloakMaterial)
+        {
+            var detectionComponent =
+                gameObject.GetComponent<DetectableObject>();
+            if (detectionComponent != null)
+                detectionComponent.isVisuallyDetectable = false;
+            var meshRenderer = transform.Find("Body").GetComponent<MeshRenderer>();
+            oldMaterial = meshRenderer.material;
+            meshRenderer.material = cloakMaterial;
+        }
+
+        private void OnDisable()
+        {
+            var detectionComponent =
+                gameObject.gameObject.GetComponent<DetectableObject>();
+            if (detectionComponent != null)
+                detectionComponent.isVisuallyDetectable = true;
+            var meshRenderer = transform.Find("Body").GetComponent<MeshRenderer>();
+            meshRenderer.material = oldMaterial;
+        }
     }
 }
