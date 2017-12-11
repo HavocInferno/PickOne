2897e46aaf344ad339933d0bc9d68c7b079e5d1b
Paul Preissner <p001@preissner-muc.de>
2017-12-11 15:58:18 +0100

    master VR UI tweaks

---
+++ Assets/VRUIFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRUIFollow : MonoBehaviour {
+
+	public Transform followedUIObj;
+	public float dampen = 3f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Slerp (
+									transform.rotation, 
+									Quaternion.LookRotation (new Vector3 (followedUIObj.forward.x, 0, followedUIObj.forward.z)), 
+									Time.deltaTime * dampen);
+	}
+}
