6dd49c7d24f599a2b5a56e07b3ab72fa0327b8ef
Paul Preissner HOME <p001@preissner-muc.de>
2017-12-11 19:44:46 +0100

    some more VR UI tweaks
    endscreen VR UI appears to work as intended
    lobby now informs HMD wearer to return to flat screen

--- Assets/VRUIFollow.cs
+++ Assets/VRUIFollow.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class VRUIFollow : MonoBehaviour {
 
 	public Transform followedUIObj;
+	public Vector3 posOffset;
 	public float dampen = 3f;
-	
+
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
+		transform.position = followedUIObj.position + posOffset;
 		transform.rotation = Quaternion.Slerp (
 									transform.rotation, 
 									Quaternion.LookRotation (new Vector3 (followedUIObj.forward.x, 0, followedUIObj.forward.z)), 
