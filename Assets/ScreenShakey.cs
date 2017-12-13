6009f70bdf6d127a3b333fa0f8d2b42fa616397b
Paul Preissner <p001@preissner-muc.de>
2017-12-13 22:23:08 +0100

    screenshaaaaaake. wip though
    screenshake. works in principle, but needs to be merged and tweaked with
    CrawlerCam for final result.

---
+++ Assets/ScreenShakey.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakey : MonoBehaviour {
+
+	public List<Screenshaker> shakeybakeys = new List<Screenshaker>();
+
+	public float cumulativeShakeyStrength;
+	public float maxCumStrength;
+	private Vector3 originalPos;
+	private bool shaking = false;
+
+	void Start() {
+		originalPos = transform.localPosition;
+	}
+
+	void Update() {
+
+		//calculate total shake strength
+		cumulativeShakeyStrength = 0f;
+		foreach (Screenshaker ss in shakeybakeys) {
+			float dist = Vector3.Magnitude (transform.position - ss.origin.position);
+			if(dist < ss.maxShakeDistance) {
+				cumulativeShakeyStrength += ss.shakeyStrength * (1 - dist / ss.maxShakeDistance);
+			}
+		}
+		cumulativeShakeyStrength = Mathf.Clamp (cumulativeShakeyStrength, 0f, maxCumStrength);
+			
+		//actual camera shake part
+		if (cumulativeShakeyStrength > 0) {
+			transform.localPosition += Random.insideUnitSphere * cumulativeShakeyStrength * Time.deltaTime;
+		}
+		transform.localPosition = Vector3.Lerp (transform.localPosition, originalPos, Time.deltaTime);
+	}
+}
---
+++ Assets/Screenshaker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshaker : MonoBehaviour {
+
+	public float duration;
+	public bool destroyAfterDuration;
+	public Transform origin;
+	public float shakeyStrength;
+	float shakenibba;
+	public float maxShakeDistance;
+
+	void Start() {
+		shakenibba = shakeyStrength;
+		FindObjectOfType<ScreenShakey> ().shakeybakeys.Add (this);
+
+		if (duration > 0) {
+			if (destroyAfterDuration)
+				Destroy (this, duration);
+		}
+	}
+
+	void Update() {
+		shakeyStrength -= shakenibba * Time.deltaTime/duration;
+	}
+
+	void OnDestroy() {
+		FindObjectOfType<ScreenShakey> ().shakeybakeys.Remove(this);
+	}
+}
