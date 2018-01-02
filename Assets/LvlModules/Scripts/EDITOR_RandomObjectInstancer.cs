655e57d6be3b021a5b478ce58f86c0eae91d52b0
Paul Preissner <p001@preissner-muc.de>
2018-01-02 19:59:39 +0100

    starting on some tools for easier room prefab creation

---
+++ Assets/LvlModules/Scripts/EDITOR_RandomObjectInstancer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RandomObjectInstancer))]
+[CanEditMultipleObjects]
+public class EDITOR_RandomObjectInstancer : Editor {
+
+	SerializedProperty roi_objectType;
+	SerializedProperty roi_maxSize;
+	SerializedProperty roi_objects;
+	SerializedProperty roi_rolledObject;
+
+	private static GUIContent
+		rollButtonContent = new GUIContent ("Roll the dice!", "Randomly spawn an item from the list");
+
+	void OnEnable() {
+		roi_objectType = serializedObject.FindProperty ("objectType");
+		roi_maxSize = serializedObject.FindProperty ("maxSize");
+		roi_objects = serializedObject.FindProperty ("objects");
+		roi_rolledObject = serializedObject.FindProperty ("rolledObject");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update ();
+		//EditorGUILayout.LabelField ("skrrraaa");
+		EditorGUILayout.PropertyField (roi_objectType);
+		EditorGUILayout.PropertyField (roi_maxSize);
+		//EditorGUILayout.PropertyField (roi_objects,true);
+		CustomEditorList.Show(roi_objects);
+
+		if (GUILayout.Button (rollButtonContent)) {
+			foreach (Object roi in targets) {
+				((RandomObjectInstancer)roi).rolledObject = ((RandomObjectInstancer)roi).objects [Random.Range (0, ((RandomObjectInstancer)roi).objects.Count)];
+			}
+			//roi_rolledObject.objectReferenceValue = roi_objects.GetArrayElementAtIndex(Random.Range(0, roi_objects.arraySize)).objectReferenceValue;
+			serializedObject.ApplyModifiedProperties ();
+			serializedObject.Update ();
+			foreach (Object roi in targets) {
+				((RandomObjectInstancer)roi).showObject ();
+			}
+		}
+		EditorGUILayout.PropertyField (roi_rolledObject);
+		serializedObject.ApplyModifiedProperties ();
+
+		//base.OnInspectorGUI ();
+	}
+}
---
+++ Assets/LvlModules/Scripts/RandomObjectInstancer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class RandomObjectInstancer : MonoBehaviour {
+
+	public enum ObjectType {
+		MISC,
+		LIGHT,
+		FURNITURE,
+		DEBRIS
+	}
+	public ObjectType objectType;
+	public Vector3 maxSize = Vector3.one;
+	public List<GameObject> objects = new List<GameObject>();
+	public GameObject rolledObject;
+	private GameObject instance;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showObject() {
+		if (instance)
+			DestroyImmediate (instance);
+		instance = Instantiate (rolledObject, transform.position, Quaternion.identity);
+	}
+
+	#if UNITY_EDITOR
+	void OnDrawGizmos()
+	{
+		Gizmos.color = new Color (1f, 0f, 0f, 0.3f);
+		Gizmos.DrawCube(transform.position, maxSize);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireCube(transform.position, maxSize);
+
+		/*Gizmos.DrawLine(transform.position - Vector3.up * 1f, transform.position + Vector3.up * 1f);
+		Gizmos.DrawLine(transform.position - Vector3.forward * 1f, transform.position + Vector3.forward * 1f);
+		Gizmos.DrawLine(transform.position - Vector3.left * 1f, transform.position + Vector3.left * 1f);*/
+	}
+	#endif
+}
