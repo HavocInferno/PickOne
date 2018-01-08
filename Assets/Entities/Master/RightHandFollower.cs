f7fc043213cbdc740d9b9054dce3b33e50d11ccb
Paul Preissner HOME <p001@preissner-muc.de>
2018-01-08 22:37:56 +0100

    Hummus bin Pummus
    So, now we got a better master all around. more functionality, better
    visualization
    background city level loads in properly (still needs a final lightmap
    pass)
    highlighting of enemies via detection skill works again (and now better
    than before. jesus, 200 LOC for that??)
    what else? idk. day over.

--- Assets/Entities/Master/RightHandFollower.cs
+++ Assets/Entities/Master/RightHandFollower.cs
@@ -11,6 +11,13 @@ public class RightHandFollower : MasterFollower
 	public Master master;
 	[SyncVar]
 	public bool isBuffed, isDebuffed;
+	public ParticleSystem buffSystem, debuffSystem;
+
+	public GameObject healOrbVis;
+	Vector3 healVisScale;
+	public GameObject fireBallVis;
+	Vector3 fireVisScale;
+
 	public Material masterMaterial;
 	public Material defaultMaterial;
 	public Material[] abilityMaterials;
@@ -19,6 +26,10 @@ public class RightHandFollower : MasterFollower
 	public Transform pingArrow, capsule;
 	public float growspeed =20;
 	public Vector3 arrowscale, capsuleScale;
+	[SyncVar]
+	float chargeFire =0; 
+	[SyncVar]
+	float chargeHeal =0; 
 
 	[SyncVar]
 	int lastItem = -1;
@@ -29,6 +40,8 @@ public class RightHandFollower : MasterFollower
 	// Use this for initialization
 	void Start()
     {
+		buffSystem.enableEmission = false; 
+		debuffSystem.enableEmission = false; 
 		arrowscale = pingArrow.localScale;
 		capsuleScale = capsule.localScale;
         if (followed.gameObject.activeInHierarchy)
@@ -39,7 +52,29 @@ public class RightHandFollower : MasterFollower
         {
 			debuff.origin = buff.origin = origin;
         }
+		initThrowables ();
     }
+	void initThrowables ()
+	{
+		fireVisScale = fireBallVis.transform.localScale;
+		healVisScale = healOrbVis.transform.localScale;
+		if (followed.gameObject.activeInHierarchy) {
+			healOrbVis.SetActive (false);
+			fireBallVis.SetActive (false);
+		}
+	}
+	void updateThrowables ()
+	{
+		if (followed.gameObject.activeInHierarchy) {
+			chargeFire = master.chargeFire;
+			chargeHeal = master.chargeHeal;
+			healOrbVis.SetActive (false);
+			fireBallVis.SetActive (false);
+		} else {
+			fireBallVis.transform.localScale = fireVisScale * chargeFire;
+			healOrbVis.transform.localScale = healVisScale * chargeHeal;
+		}
+	}
 	
 	// Update is called once per frame
 	protected override void Update()
@@ -68,19 +103,24 @@ public class RightHandFollower : MasterFollower
 			debuff.destination = debuffTarget;
 			debuff.Draw = isDebuffed;
         }
+
+		buffSystem.enableEmission = isBuffed; 
+		debuffSystem.enableEmission = isDebuffed; 
+
+		if (currentitem >= 0 && currentitem < abilityMaterials.Length)
+			masterMaterial.Lerp (masterMaterial, abilityMaterials [currentitem], Time.deltaTime);
+		else
+			masterMaterial.Lerp (masterMaterial, defaultMaterial, Time.deltaTime);
+
 		if (gripping) {
-			masterMaterial.Lerp (masterMaterial, pingMaterial, Time.deltaTime*5);
+			//masterMaterial.Lerp (masterMaterial, pingMaterial, Time.deltaTime*5);
 			pingArrow.localScale = Vector3.Lerp (pingArrow.localScale, arrowscale, Time.deltaTime*growspeed);
 			capsule.localScale = Vector3.Lerp (capsule.localScale, Vector3.zero, Time.deltaTime*growspeed);
 		} else {
-			if (currentitem >= 0 && currentitem < abilityMaterials.Length)
-				masterMaterial.Lerp (masterMaterial, abilityMaterials [currentitem], Time.deltaTime);
-			else
-				masterMaterial.Lerp (masterMaterial, defaultMaterial, Time.deltaTime);
-				
 			pingArrow.localScale = Vector3.Lerp (pingArrow.localScale, Vector3.zero, Time.deltaTime*growspeed);
 			capsule.localScale = Vector3.Lerp (capsule.localScale, capsuleScale, Time.deltaTime*growspeed);
 			
 		}
+		updateThrowables ();
     }
 }
