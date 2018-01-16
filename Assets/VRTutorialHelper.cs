ffdf6b93a931c0b708a7821c43e87f2f2e4f5e6e
Paul Preissner HOME <p001@preissner-muc.de>
2018-01-16 23:29:50 +0100

    WE GON MAKE THIS GO VIRAL

--- Assets/VRTutorialHelper.cs
+++ Assets/VRTutorialHelper.cs
@@ -5,11 +5,31 @@ using System.IO;
 
 public class VRTutorialHelper : MonoBehaviour {
 
+	bool tutOn;
 	public GameObject sign;
+	public GameObject riftPart;
+	public GameObject vivePart;
+	int vrDeviceModel = -1;
 
 	// Use this for initialization
 	void Start () {
 		LoadSetting ();
+
+		if (!tutOn)
+			return;
+		
+		QueryVRDeviceModel ();
+
+		if (vrDeviceModel == 1) {
+			vivePart.SetActive (true);
+			riftPart.SetActive (false);
+		} else if (vrDeviceModel == 2) {
+			vivePart.SetActive (false);
+			riftPart.SetActive (true);
+		} else {
+			vivePart.SetActive (false);
+			riftPart.SetActive (false);
+		}
 	}
 
 	public void LoadSetting() {
@@ -18,7 +38,27 @@ public class VRTutorialHelper : MonoBehaviour {
 		if (File.Exists (Application.persistentDataPath + "/gamesettings.json")) {
 			gameSettings = JsonUtility.FromJson<GameSettingsContainer> (File.ReadAllText (Application.persistentDataPath + "/gamesettings.json"));
 
-			sign.SetActive(gameSettings.showVRTutorial);
+			tutOn = gameSettings.showVRTutorial;
+			sign.SetActive(tutOn);
+		}
+	}
+
+	public void QueryVRDeviceModel() {
+		Debug.Log ("Querying VR Device Model");
+		if (UnityEngine.XR.XRDevice.isPresent) {
+			//isVRcapable = true;
+
+			string model = UnityEngine.XR.XRDevice.model != null ?
+				UnityEngine.XR.XRDevice.model : "";
+
+			if (model.IndexOf ("Rift") >= 0) {
+				vrDeviceModel = 2;
+			} else {
+				vrDeviceModel = 1;
+			}
+			Debug.Log ("VR headset found, model " + vrDeviceModel + "; " + model);
+		} else {
+			vrDeviceModel = -1;
 		}
 	}
 }
