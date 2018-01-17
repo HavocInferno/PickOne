66dc4536d0e4ae32ce240ecc43d104777c4c9d2c
Muhammad Inshal Uddin <inshal240@hotmail.com>
2018-01-17 21:30:50 +0100

    Added panel for ability cooldown notifications

---
+++ Assets/Entities/Crawlers/Abilities/Icon/AbilitiesPanel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(VerticalLayoutGroup))]
+public class AbilitiesPanel : MonoBehaviour
+{
+    public GameObject elementPrefab;
+    public float disappearSpeed = 400f;
+
+    private Dictionary<ActiveAbility, GameObject> _elements =
+        new Dictionary<ActiveAbility, GameObject>();
+
+    // Initializes the view
+    public void Initialize(List<ActiveAbility> abilities)
+    {
+        foreach (var ability in abilities)
+        {
+            GameObject newObject = GameObject.Instantiate(elementPrefab, transform);
+            Image image = newObject.transform.Find("EffectIcon").GetComponentInChildren<Image>();
+            image.sprite = ability.readyIcon;
+            _elements.Add(ability, newObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var ability in _elements.Keys)
+        {
+            if (ability.IsAvailable)
+            {
+                _elements[ability]
+                    .transform.Find("EffectIcon")
+                    .GetComponentInChildren<Image>().sprite = ability.readyIcon;
+            }
+            else
+            {
+                _elements[ability]
+                    .transform.Find("EffectIcon")
+                    .GetComponentInChildren<Image>().sprite = ability.unavailableIcon;
+            }
+        }
+    }
+}
