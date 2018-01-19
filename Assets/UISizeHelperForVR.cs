56222eb385378b329995028e325a3e8d6f8ee7f3
Paul Preissner HOME <p001@preissner-muc.de>
2018-01-19 12:48:58 +0100

    death effect more clear, also VR UI stuff
    maaaaybe now crawler names are bigger for the master... I hope.

---
+++ Assets/UISizeHelperForVR.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISizeHelperForVR : MonoBehaviour {
+
+	public RectTransform uiObj;
+	public float vrSize;
+
+	// Use this for initialization
+	void Start () {
+		if (FindObjectOfType<Master> () && UnityEngine.XR.XRSettings.enabled)
+			uiObj.localScale *= vrSize;
+	}
+}
