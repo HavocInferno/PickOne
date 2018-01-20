2753dd5325990ec864e9134e206488b2a43fd31d
Paul Preissner HOME <p001@preissner-muc.de>
2018-01-20 15:46:55 +0100

    scene shows objective now, plus UI scaling fixes
    scene shows a simple UI info about objective at the start of the match
    
    also changed some UI scaling modes, may show less issues with cut-off UI
    parts on uncommon screen aspect ratios now...

---
+++ Assets/ObjectiveInfoHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveInfoHelper : MonoBehaviour {
+
+	public float lifetime;
+	RectTransform selfRT;
+	bool hide;
+
+	// Use this for initialization
+	void Start () {
+		selfRT = GetComponent<RectTransform> ();
+		StartCoroutine (disableAfter (lifetime));
+	}
+
+	void Update() {
+		if (!hide)
+			return;
+
+		selfRT.anchoredPosition = new Vector2(selfRT.anchoredPosition.x, Mathf.Lerp (selfRT.anchoredPosition.y, 200f, Time.deltaTime * 3f));
+	}
+
+	IEnumerator disableAfter(float lifetime) {
+		yield return new WaitForSeconds (lifetime);
+		hide = true;
+		yield return new WaitForSeconds (5f);
+		gameObject.SetActive (false);
+	}
+}
