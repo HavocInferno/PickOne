83c73a491a8f26dbf22d1c591a4b458a40137d92
Paul Preissner HOME <p001@preissner-muc.de>
2018-01-26 07:41:03 +0100

    WHAT M9???

---
+++ Assets/CanvasRendererAlphaHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasRendererAlphaHelper : MonoBehaviour {
+
+	public float startCanvasRAlpha;
+	public Color startColor;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Image> ().canvasRenderer.SetAlpha (startCanvasRAlpha);
+		GetComponent<Image> ().color = startColor;
+	}
+}
