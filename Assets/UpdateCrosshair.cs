0e31957a52225ab8f107987965eb25291fa7d088
Muhammad Inshal Uddin <inshal240@gmail.com>
2018-01-30 17:46:56 +0100

    Restrict crosshair update to local player

--- Assets/UpdateCrosshair.cs
+++ Assets/UpdateCrosshair.cs
@@ -7,25 +7,30 @@ public class UpdateCrosshair : MonoBehaviour
     public Transform bulletSpawn;
     public LayerMask layerMask;
 
+    protected Crawler _crawler;
     protected CUI_crosshair _crosshair;
     
     void Start ()
     {
         // TODO: Find alterantive
         _crosshair = Object.FindObjectOfType<CUI_crosshair>();
+        _crawler = GetComponentInParent<Crawler>();
     }
 
     protected void Update()
     {
-        // Figure out where the gun is aiming
-        var ray = new Ray(bulletSpawn.position, bulletSpawn.forward);
-
-        RaycastHit rayHit;
-        if (Physics.Raycast(ray, out rayHit, 100, layerMask))
+        if (_crawler.isLocalPlayer)
         {
-            // Rotate the gun to point to the... point
-            var crosshairTransform = _crosshair.GetComponent<RectTransform>();
-            crosshairTransform.position = Camera.main.WorldToScreenPoint(rayHit.point);
+            // Figure out where the gun is aiming
+            var ray = new Ray(bulletSpawn.position, bulletSpawn.forward);
+
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100, layerMask))
+            {
+                // Rotate the gun to point to the... point
+                var crosshairTransform = _crosshair.GetComponent<RectTransform>();
+                crosshairTransform.position = Camera.main.WorldToScreenPoint(rayHit.point);
+            }
         }
     }
 }
