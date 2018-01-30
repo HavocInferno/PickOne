de1a700e0b97f9079925dd52fa065161359abfff
RRRRRRRRRRRRRRRRRRRRRRRR <jonas.a.mayer.95@googlemail.com>
2018-01-30 15:46:18 +0100

    Fix your shit
    kjdhrgkgdfg

--- Assets/UIMasterIndicator.cs
+++ Assets/UIMasterIndicator.cs
@@ -21,22 +21,30 @@ public class UIMasterIndicator : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 masterScreenPoint = Camera.main.WorldToViewportPoint (master.transform.position);
-		bool onScreen = (masterScreenPoint.z > 0
-						&& masterScreenPoint.x > (0f+onScreenRim)
-						&& masterScreenPoint.x < (1f-onScreenRim) 
-						&& masterScreenPoint.y > (0f+onScreenRim) 
-						&& masterScreenPoint.y < (1f-onScreenRim));
-		if (onScreen) {
-			//indicatorArrow.GetComponent<Renderer> ().material.color = Color.Lerp (indicatorArrow.GetComponent<Renderer> ().material.color, Color.clear, Time.deltaTime * fadeSpeed);
-			indicatorArrow.transform.localPosition = Vector3.Lerp (indicatorArrow.transform.localPosition, arrowOffset, Time.deltaTime * fadeSpeed);
-			indicatorText.color = Color.Lerp (indicatorText.color, Color.clear, Time.deltaTime * fadeSpeed);
-		} else {
-			//indicatorArrow.GetComponent<Renderer> ().material.color = Color.Lerp (indicatorArrow.GetComponent<Renderer> ().material.color, arrowCol, Time.deltaTime * fadeSpeed);
-			indicatorArrow.transform.localPosition = Vector3.Lerp (indicatorArrow.transform.localPosition, Vector3.zero, Time.deltaTime * fadeSpeed);
-			indicatorText.color = Color.Lerp (indicatorText.color, textCol, Time.deltaTime * fadeSpeed);
-		}
+        if (master != null)
+        {
+            Vector3 masterScreenPoint = Camera.main.WorldToViewportPoint(master.transform.position);
+            bool onScreen = (masterScreenPoint.z > 0
+                            && masterScreenPoint.x > (0f + onScreenRim)
+                            && masterScreenPoint.x < (1f - onScreenRim)
+                            && masterScreenPoint.y > (0f + onScreenRim)
+                            && masterScreenPoint.y < (1f - onScreenRim));
+            if (onScreen)
+            {
+                //indicatorArrow.GetComponent<Renderer> ().material.color = Color.Lerp (indicatorArrow.GetComponent<Renderer> ().material.color, Color.clear, Time.deltaTime * fadeSpeed);
+                indicatorArrow.transform.localPosition = Vector3.Lerp(indicatorArrow.transform.localPosition, arrowOffset, Time.deltaTime * fadeSpeed);
+                indicatorText.color = Color.Lerp(indicatorText.color, Color.clear, Time.deltaTime * fadeSpeed);
+            }
+            else
+            {
+                //indicatorArrow.GetComponent<Renderer> ().material.color = Color.Lerp (indicatorArrow.GetComponent<Renderer> ().material.color, arrowCol, Time.deltaTime * fadeSpeed);
+                indicatorArrow.transform.localPosition = Vector3.Lerp(indicatorArrow.transform.localPosition, Vector3.zero, Time.deltaTime * fadeSpeed);
+                indicatorText.color = Color.Lerp(indicatorText.color, textCol, Time.deltaTime * fadeSpeed);
+            }
 
-		indicatorArrow.transform.LookAt (master.transform);
+            indicatorArrow.transform.LookAt(master.transform);
+        }
+        else
+            Debug.LogError("No master to point to.");
 	}
 }
