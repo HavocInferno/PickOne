776e33e6155b494435647c4da28ea114bbcfab38
Paul Preissner HOME <p001@preissner-muc.de>
2018-02-01 13:19:32 +0100

    small fire balance fix, enemy sound test
    - robots have a simple stupid sound effect now, scorpions still dont.
    
    - fire may be a little more fair now
    
    - controls tab in menu updated to reflect latest changes

--- Assets/DeathCamMover.cs
+++ Assets/DeathCamMover.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class DeathCamMover : MonoBehaviour {
 
 	public Transform deathCamT;
+	public float movSpeed = 1f;
 	
 	// Update is called once per frame
 	void Update () {
-		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, deathCamT.position, Time.deltaTime * 5f);
-		Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, deathCamT.rotation, Time.deltaTime * 5f);
+		Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, deathCamT.position, Time.deltaTime * movSpeed);
+		Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, deathCamT.rotation, Time.deltaTime * movSpeed);
 	}
 }
